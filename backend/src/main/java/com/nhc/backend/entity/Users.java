a5a02ade2ce701f9fe86adc0340a5f0e8830d228
hp657 <junlee5070@gmail.com>
2024-07-26 09:59:54 +0900

    유저 엔티티

--- backend/src/main/java/com/nhc/backend/entity/Users.java
+++ backend/src/main/java/com/nhc/backend/entity/Users.java
@@ -1,2 +1,18 @@
-package com.nhc.backend.entity;public class Users {
+package com.nhc.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
 }
