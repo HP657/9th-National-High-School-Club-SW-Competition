92846e1b8c872c1f2c6d943f551e0e26ae3ddbba
hp657 <junlee5070@gmail.com>
2024-07-30 09:59:56 +0900

    App.js

--- app/App.js
+++ app/App.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import FirstPage from './Component/FirstPage';
-import Intro from './Component/Intro';
+import ChatRoom from './Component/ChatRoom';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Intro" headerMode="none">
-        <Stack.Screen name="Intro" component={Intro} />
+      {/* <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="FirstPage" component={FirstPage} />
-      </Stack.Navigator>
+      </Stack.Navigator> */}
+      <ChatRoom />
     </NavigationContainer>
   );
 };
