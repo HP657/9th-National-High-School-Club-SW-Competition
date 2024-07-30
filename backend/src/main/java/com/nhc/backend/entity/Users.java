77554e8966edd0dba29dd00ca62a1aa93bf3cc0a
hp657 <junlee5070@gmail.com>
2024-07-30 10:23:15 +0900

    유저 엔티티

--- backend/src/main/java/com/nhc/backend/entity/Users.java
+++ backend/src/main/java/com/nhc/backend/entity/Users.java
@@ -3,7 +3,6 @@ package com.nhc.backend.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-
 @Entity
 @Getter
 @Setter
@@ -15,4 +14,18 @@ public class Users {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    public Users(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 }
