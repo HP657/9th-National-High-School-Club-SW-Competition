0008d8f7d357ebf6e9f6ae53c850ce859123db39
hp657 <junlee5070@gmail.com>
2024-07-30 10:22:07 +0900

    회원관리 api

--- backend/src/main/java/com/nhc/backend/api/AuthApiController.java
+++ backend/src/main/java/com/nhc/backend/api/AuthApiController.java
@@ -1,3 +1,37 @@
 package com.nhc.backend.api;
+
+import com.nhc.backend.dto.In.SignInDto;
+import com.nhc.backend.dto.In.SignUpDto;
+import com.nhc.backend.dto.Out.Response;
+import com.nhc.backend.service.AuthService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthApiController {
+
+    @Autowired
+    private AuthService authService;
+
+    //회원가입
+    @PostMapping("/signup")
+    public ResponseEntity<Response<String>> signUpUser(@RequestBody SignUpDto signupDto) {
+        return authService.signup(signupDto).toResponseEntity();
+    }
+
+    //로그인
+    @PostMapping("/signin")
+    public ResponseEntity<Response<String>> signInUser(@RequestBody SignInDto signinDto, HttpServletRequest request) {
+        return authService.signin(signinDto, request).toResponseEntity();
+    }
+
+    //로그아웃
+    @GetMapping("/logout")
+    public ResponseEntity<Response<String>> logOutUser(HttpServletRequest request) {
+        return authService.logout(request).toResponseEntity();
+    }
+
 }
