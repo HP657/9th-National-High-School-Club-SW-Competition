2135e4448ace80431cc627c272bc0379555d5168
hp657 <junlee5070@gmail.com>
2024-07-30 10:22:57 +0900

    회원가입 dto

--- backend/src/main/java/com/nhc/backend/dto/In/SignUpDto.java
+++ backend/src/main/java/com/nhc/backend/dto/In/SignUpDto.java
@@ -1,2 +1,10 @@
-package com.nhc.backend.dto.In;public class SignUpDto {
+package com.nhc.backend.dto.In;
+
+import lombok.Data;
+
+@Data
+public class SignUpDto {
+    private String username;
+    private String email;
+    private String password;
 }
