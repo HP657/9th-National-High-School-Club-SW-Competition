48c0394211656c4b98bd0892de13b4e8fce6c051
hp657 <junlee5070@gmail.com>
2024-07-30 15:35:06 +0900

    웹소켓 설정

--- backend/src/main/java/com/nhc/backend/config/WebSocket.java
+++ backend/src/main/java/com/nhc/backend/config/WebSocket.java
@@ -13,7 +13,7 @@ public class WebSocket implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("*");
     }
 
     @Override
