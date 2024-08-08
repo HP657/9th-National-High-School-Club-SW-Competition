fb9c780bfa5bbb3f2bfda8811408c7d5e495a2fb
hp657 <junlee5070@gmail.com>
2024-08-08 13:20:05 +0900

    채팅방 컨트롤러 따로 만듬

--- backend/src/main/java/com/nhc/backend/api/ChatRoomApiController.java
+++ backend/src/main/java/com/nhc/backend/api/ChatRoomApiController.java
@@ -5,10 +5,6 @@ import com.nhc.backend.entity.Messages;
 import com.nhc.backend.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,15 +18,8 @@ public class ChatRoomApiController {
         return ResponseEntity.ok(messageService.getMessagesByChatRoomId(chatRoomId));
     }
 
-    @MessageMapping("/chat.send/{roomId}")
-    @SendTo("/topic/public/{roomId}")
-    public Messages sendMessage(@Payload MessageDto messageDto, @DestinationVariable String roomId) {
-        return messageService.saveMessage(messageDto);
-    }
-
-    @PostMapping("/{chatRoomId}/send")
-    public ResponseEntity<?> receiveMessageFromFlask(@PathVariable Long chatRoomId, @RequestBody MessageDto messageDto) {
-        messageDto.setChatRoomId(chatRoomId);
+    @PostMapping("/send")
+    public ResponseEntity<?> receiveMessageFromFlask( @RequestBody MessageDto messageDto) {
         Messages savedMessage = messageService.saveMessage(messageDto);
         return ResponseEntity.ok(savedMessage);
     }
