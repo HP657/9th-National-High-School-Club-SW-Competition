ffee62d4d6b6077d5e60cafab7080efa959e78a3
hp657 <junlee5070@gmail.com>
2024-08-20 16:08:20 +0900

    웹소켓 관련 삭제

--- backend/src/main/java/com/nhc/backend/repository/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nhc.backend.repository;
-import com.nhc.backend.entity.Messages;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface MessageRepository extends JpaRepository<Messages, Long> {
-    List<Messages> findByChatRoomIdOrderByTimestampAsc(Long chatRoomId);
-}
